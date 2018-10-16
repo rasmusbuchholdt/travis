231beefe9e85ea669544e7547bdfc2cb037c48fe
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2018-10-16 13:21:47 +0200

    Introduce Spotify API player methods to Spotify class

--- dist/modules/spotify.js
+++ dist/modules/spotify.js
@@ -1,11 +1,83 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var spotifyStrategy = require("passport-spotify").Strategy;
+var request = require('request-promise');
 var config = require("../../config/app.json");
 var Spotify = /** @class */ (function () {
-    function Spotify(token) {
-        this.token = token;
+    function Spotify(accessToken) {
+        this.accessToken = accessToken;
     }
+    ;
+    Spotify.prototype.controlPlayback = function (action) {
+        switch (action) {
+            case "previous":
+                this.previous(this.accessToken);
+                break;
+            case "next":
+                this.next(this.accessToken);
+                break;
+            case "pause":
+                this.pause(this.accessToken);
+                break;
+            case "resume":
+                this.resume(this.accessToken);
+                break;
+            default:
+                break;
+        }
+    };
+    Spotify.prototype.previous = function (accessToken) {
+        var options = {
+            method: "POST",
+            uri: "https://api.spotify.com/v1/me/player/previous",
+            headers: {
+                Authorization: " Bearer " + accessToken
+            }
+        };
+        request(options)
+            .catch(function (error) {
+            console.error(error);
+        });
+    };
+    Spotify.prototype.next = function (accessToken) {
+        var options = {
+            method: "POST",
+            uri: "https://api.spotify.com/v1/me/player/next",
+            headers: {
+                Authorization: " Bearer " + accessToken
+            }
+        };
+        request(options)
+            .catch(function (error) {
+            console.error(error);
+        });
+    };
+    Spotify.prototype.resume = function (accessToken) {
+        var options = {
+            method: "PUT",
+            uri: "https://api.spotify.com/v1/me/player/play",
+            headers: {
+                Authorization: " Bearer " + accessToken
+            }
+        };
+        request(options)
+            .catch(function (error) {
+            console.error(error);
+        });
+    };
+    Spotify.prototype.pause = function (accessToken) {
+        var options = {
+            method: "PUT",
+            uri: "https://api.spotify.com/v1/me/player/pause",
+            headers: {
+                Authorization: " Bearer " + accessToken
+            }
+        };
+        request(options)
+            .catch(function (error) {
+            console.error(error);
+        });
+    };
     Spotify.authStrategy = function () {
         return new spotifyStrategy({
             clientID: process.env.spotityClientID || config.spotityClientID,
--- src/modules/spotify.ts
+++ src/modules/spotify.ts
@@ -1,13 +1,89 @@
 let spotifyStrategy = require("passport-spotify").Strategy;
+let request = require('request-promise');
 
 let config = require("../../config/app.json");
 
 export class Spotify {
 
-    private token: string;
+    private accessToken: string;
 
-    constructor(token: string) {
-        this.token = token;
+    constructor(accessToken: string) {
+        this.accessToken = accessToken;
+    };
+
+    controlPlayback(action: string) {
+        switch (action) {
+            case "previous":
+                this.previous(this.accessToken);
+                break;
+            case "next":
+                this.next(this.accessToken);
+                break; 
+            case "pause":
+                this.pause(this.accessToken);
+                break;   
+            case "resume":
+                this.resume(this.accessToken);
+                break;    
+            default:
+                break;
+        }
+    }
+
+    private previous(accessToken: string) {
+        let options: {} = {
+            method: "POST",
+            uri: "https://api.spotify.com/v1/me/player/previous",
+            headers: {
+                Authorization: ` Bearer ${accessToken}`
+            }
+        };
+        request(options)
+        .catch(error => {
+            console.error(error);
+        });
+    }
+
+    private next(accessToken: string) {
+        let options: {} = {
+            method: "POST",
+            uri: "https://api.spotify.com/v1/me/player/next",
+            headers: {
+                Authorization: ` Bearer ${accessToken}`
+            }
+        };
+        request(options)
+        .catch(error => {
+            console.error(error);
+        });
+    }
+
+    private resume(accessToken: string) {
+        let options: {} = {
+            method: "PUT",
+            uri: "https://api.spotify.com/v1/me/player/play",
+            headers: {
+                Authorization: ` Bearer ${accessToken}`
+            }
+        };
+        request(options)
+        .catch(error => {
+            console.error(error);
+        });
+    }
+
+    private pause(accessToken: string) {
+        let options: {} = {
+            method: "PUT",
+            uri: "https://api.spotify.com/v1/me/player/pause",
+            headers: {
+                Authorization: ` Bearer ${accessToken}`
+            }
+        };
+        request(options)
+        .catch(error => {
+            console.error(error);
+        });
     }
 
     static authStrategy() {

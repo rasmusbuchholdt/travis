5410a6e151552fd6fc3dcb751c90e7811d1a117d
Rasmus Buchholdt <RasmusBuchholdt@users.noreply.github.com>
2018-10-18 23:56:42 +0200

    Force build script to run in correct order

--- src/build.ts
+++ src/build.ts
@@ -1,27 +1,36 @@
 let fs = require("fs-extra");
 
-console.log("Starting build script..");
-fs.copy(__dirname + "/../src/client", __dirname + "/client")
+function copyClientDirectory() {
+    fs.copy(__dirname + "/../src/client", __dirname + "/client")
     .then(() => {
         console.log(".. Successfully copied client directory!");
+        emtpyClientCss();
     })
     .catch(error => {
         console.error(error)
     });
+}
 
-
-fs.emptyDir(__dirname + "/client/css")
+function emtpyClientCss() {
+    fs.emptyDir(__dirname + "/client/css")
     .then(() => {
         console.log(".. Successfully deleted client css!");
+        //emptyClientJs();
     })
     .catch(error => {
         console.error(error)
     });
+}
 
-// fs.emptyDir(__dirname + "/client/js")
-//     .then(() => {
-//         console.log(".. Successfully deleted client js!");
-//     })
-//     .catch(error => {
-//         console.error(error)
-//     });
+function emptyClientJs() {
+    fs.emptyDir(__dirname + "/client/js")
+    .then(() => {
+        console.log(".. Successfully deleted client js!");
+    })
+    .catch(error => {
+        console.error(error)
+    });
+}
+
+console.log("Starting build script..");
+copyClientDirectory();
